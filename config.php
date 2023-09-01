66bbe8d393b839ee7f55c001d0f8a3808092d436
Tanish Raj <help.cttricks@gmail.com>
2023-09-01 15:05:29 +0530

    Dashboard added with sime helpher files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set("Asia/Kolkata");
+
+define('VERSION', time());
+define('MAX_FILE_SIZE', 1); //1MB
+define('MAX_SUB_FOLDERS', 10); //10 SubFolders/Folder 
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include_once('config.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('common/meta.html'); ?>
+    <?php include_once('common/links.html'); ?>
+    <title>Asset Harbor</title>
+    <link rel="stylesheet" href="./stylesheets/common.css?v=<?= VERSION; ?>" />
+    <link rel="stylesheet" href="./stylesheets/dashboard.css?v=<?= VERSION; ?>" />
+</head>
+<body>
+    <!-- Menu & Main -->
+    <section class="container">
+        <?php include_once('pages/menu.html'); ?>
+        <?php include_once('pages/main.html'); ?>
+    </section>
+    <!-- File Info Container & Viewer -->
+    <!-- Footer -->
+</body>
+</html>
